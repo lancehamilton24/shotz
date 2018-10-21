1348f79499ad94f64cbeb60494923883212a6262
Lance Hamilton <lance.hamilton24@gmail.com>
2018-10-21 08:59:46 -0500

    added location data and printed location JSON data to the dom

--- javascripts/components/locationsComponent.js
+++ javascripts/components/locationsComponent.js
@@ -1,1 +1,29 @@
-console.log("locationsComponent.js");
+const locationBuilder = locationArray => {
+  let domString = "";
+  locationArray.forEach(location => {
+    domString += `
+      <div class="col-md-3 location-card" id="locations">
+      <div class="card">
+       <img class="card-img-top" src="${location.locationImage}" alt="">
+        <div class="card-body">
+           <h2 class="card-title">${location.name}</h2>
+           <h4>${location.locationAddress}</h4>
+          <p>${location.time}</p>
+      </div>
+    </div>
+  </div>
+          `;
+  });
+  // Write to the available div
+  $("#shotLocations").append(domString);
+  // bindEvents();
+};
+
+$.get("../db/locations.json")
+  .done(data => {
+    console.log(data);
+    locationBuilder(data.locations);
+  })
+  .fail(error => {
+    console.error(error);
+  });
--- javascripts/components/movieComponent.js
+++ javascripts/components/movieComponent.js
@@ -2,21 +2,21 @@ const movieBuilder = movieArray => {
   let domString = "";
   movieArray.forEach(movie => {
     domString += `
-    <div class="col-smd movie-card" id="movies">
+    <div class="col-md movie-card" id="movies">
     <div class="card">
-     <img class="card-img-top" src="${movie.movieImage}" alt="">;
+     <img class="card-img-top" src="${movie.movieImage}" alt="">
       <div class="card-body">
          <h2 class="card-title">${movie.name}</h2>
          <h4>${movie.genre}</h4>
          <h4>${movie.releaseDate}</h4>
         <p>${movie.description}</p>
-    </div>;
-  </div>;
-</div>;
+    </div>
+  </div>
+</div>
         `;
   });
   // Write to the available div
-  $("#movieTitle").html(domString);
+  $("#movieTitle").append(domString);
   // bindEvents();
 };
 
