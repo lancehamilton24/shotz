429f8a4409f0cac96008c197ba1c61f0f6c5fc91
Lance Hamilton <lance.hamilton24@gmail.com>
2018-10-21 12:27:38 -0500

    added event listener to filter buttons

--- javascripts/events.js
+++ javascripts/events.js
@@ -1,1 +1,19 @@
-console.log("events.js");
+$("#morning").on("click", e => {
+  e.preventDefault();
+  console.log("morning");
+});
+
+$("#afternoon").on("click", e => {
+  e.preventDefault();
+  console.log("afternoon");
+});
+
+$("#evening").on("click", e => {
+  e.preventDefault();
+  console.log("evening");
+});
+
+$("#dark").on("click", e => {
+  e.preventDefault();
+  console.log("After dark");
+});
