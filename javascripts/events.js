66dc2afed4ca8cdcac7e181803d9f2f5b5343829
Lance Hamilton <lance.hamilton24@gmail.com>
2018-10-22 17:52:59 -0500

    finalized search filter

--- javascripts/events.js
+++ javascripts/events.js
@@ -1,26 +1,42 @@
 // import { sortLocations } from "./components/locationsComponent.js";
 
-const sortEvents = () => {
-  $("#morningButton").on("click", e => {
-    e.preventDefault();
-    $(locations).toggle("#morning");
-    console.log("morning");
-  });
+// const sortEvents = () => {
+//   $("#morningButton").on("click", e => {
+//     e.preventDefault();
+//     $(locations).toggle("#morning");
+//     console.log("morning");
+//   });
 
-  $("#afternoonButton").on("click", e => {
-    e.preventDefault();
-    console.log("afternoon");
-  });
+//   $("#afternoonButton").on("click", e => {
+//     e.preventDefault();
+//     console.log("afternoon");
+//   });
 
-  $("#eveningButton").on("click", e => {
-    e.preventDefault();
-    console.log("evening");
-  });
+//   $("#eveningButton").on("click", e => {
+//     e.preventDefault();
+//     console.log("evening");
+//   });
 
-  $("#darkButton").on("click", e => {
-    e.preventDefault();
-    console.log("after dark");
-  });
-};
+//   $("#darkButton").on("click", e => {
+//     e.preventDefault();
+//     console.log("after dark");
+//   });
+// };
 
-sortEvents();
+$(document).ready(function() {
+  $("#locationSearch").on("keyup", function() {
+    var value = $(this)
+      .val()
+      .toLowerCase();
+    $(".location-card").filter(function() {
+      $(this).toggle(
+        $(this)
+          .text()
+          .toLowerCase()
+          .indexOf(value) > -1
+      );
+    });
+  });
+});
+
+// sortEvents();
